c1a460f05551ff4048275834deffc7d1778efe78
Petr Kohut <me@petrkohut.cz>
2015-04-22 12:01:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -z $PS1 ]; then
-  echo "The installation of dotfiles can run only in the interactive shell mode."
-else
+if tty -s; then
   git clone https://github.com/PetrKohut/dotfiles.git ~/.tempdotfiles --depth 1
   rm -rf ~/.tempdotfiles/.git
   mv ~/.tempdotfiles/.[^.]* ~/
   rm -rf ~/.tempdotfiles
+else
+  echo "ERROR: The installation of dotfiles can run only in the interactive shell mode."
 fi
